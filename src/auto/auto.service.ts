b90c927bfb6b9fa59fc09cc61142cb795c4ef21c
李大毛 <dev@darmau.design>
2023-05-19 11:17:11 +0800

    Update auto.service.ts

--- src/auto/auto.service.ts
+++ src/auto/auto.service.ts
@@ -22,7 +22,6 @@ export class AutoService {
 
   // 获取所有website，分别将url传入updateArticlesByWebsite方法, 每4小时执行一次
   @Cron('0 0 */4 * * *')
-  // @Cron(CronExpression.EVERY_30_MINUTES)
   async updateArticle() {
     // 获取百度API token并传入，减少重复获取token的次数
     const token = await getBaiduToken();
@@ -49,7 +48,7 @@ export class AutoService {
   }
 
   // 一个月进行一次的任务。遍历所有article，检测是否可访问，不可访问的进行标记。
-  @Cron('0 0 0 1 * *')
+  @Cron('0 3 0 1 * *')
   async checkArticles() {
     const articles = await this.articleModel.find();
     this.logger.log('Start check ' + articles.length + ' articles');
@@ -78,20 +77,20 @@ export class AutoService {
   }
 
   // 每天凌晨3点执行一次，计算网站和文章的数量，写入数据库
-  // @Cron('0 0 3 * * *')
-  // async updateStatistics() {
-  //   const date = new Date();
-  //   const websitesCount = await this.websiteModel.estimatedDocumentCount();
-  //   const articlesCount = await this.articleModel.estimatedDocumentCount();
-  //   const inaccessibleArticlesCount = await this.articleModel.countDocuments({ crawl_error: { $gte: 1 } });
+  @Cron('0 0 3 * * *')
+  async updateStatistics() {
+    const date = new Date();
+    const websitesCount = await this.websiteModel.estimatedDocumentCount();
+    const articlesCount = await this.articleModel.estimatedDocumentCount();
+    const inaccessibleArticlesCount = await this.articleModel.countDocuments({ crawl_error: { $gte: 1 } });
 
-  //   const todayStatistic = await new this.statisticModel({
-  //     date: date,
-  //     website_count: websitesCount,
-  //     article_count: articlesCount,
-  //     inaccessible_article: inaccessibleArticlesCount,
-  //   });
-  //   await todayStatistic.save();
-  //   return this.logger.log('Update statistics success');
-  // }
+    const todayStatistic = await new this.statisticModel({
+      date: date,
+      website_count: websitesCount,
+      article_count: articlesCount,
+      inaccessible_article: inaccessibleArticlesCount,
+    });
+    await todayStatistic.save();
+    return this.logger.log('Update statistics success');
+  }
 }
