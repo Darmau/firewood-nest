5cf0cf19c7a49ec38559e7206d1ab5df309889a8
李大毛 <dev@darmau.design>
2023-06-01 13:45:02 +0800

    fix: 如果没有日期，则用现在时间代替

--- src/common/convert-date.ts
+++ src/common/convert-date.ts
@@ -1,4 +1,5 @@
 export default function convertToISOString(dateString) {
+  if(!dateString) return new Date().toISOString();
   const isoDate = new Date(dateString).toISOString();
   return new Date(isoDate);
 }
