9b8268965b6918ea365902c7ddcf343178ca3bc6
李大毛 <dev@darmau.design>
2023-08-09 12:03:28 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 1216;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(1994, "0.0.0.0");
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
