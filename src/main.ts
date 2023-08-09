d9d7c3eaad99177c9eab846104a900d514958de8
李大毛 <dev@darmau.design>
2023-08-09 11:58:45 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(1994);
+  await app.listen(1994, "0.0.0.0");
 }
 bootstrap();
