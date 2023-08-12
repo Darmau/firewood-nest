f62b3068c8d4da8086c8a11b01ca78701d4e3b2e
李大毛 <dev@darmau.design>
2023-08-12 18:56:03 +0800

    add dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-const port = process.env.PORT || 1216;
+const port = process.env.PORT || 1994;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
