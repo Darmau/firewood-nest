7fd137d5da952614aab09635d0dd81ac50f1e719
李大毛 <dev@darmau.design>
2023-08-26 17:48:53 +0800

    fix: 增加参数验证管道

---
+++ src/pipe/positiveInt.pipe.ts
@@ -0,0 +1,16 @@
+import {ArgumentMetadata, Injectable, PipeTransform} from "@nestjs/common";
+
+@Injectable()
+export class PositiveIntPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata): any {
+    if (metadata.type === 'query') {
+      if (metadata.data === 'limit' && value < 1) {
+        return 10
+      }
+      if (metadata.data === 'page' && value < 1) {
+        return 1
+      }
+    }
+    return value;
+  }
+}
