70e6b36bb52a04ff99427dd44e2d2b328b5ff5b3
李大毛 <dev@darmau.design>
2023-09-03 21:50:34 +0800

    fix: logger

--- src/common/cloudflare-image.ts
+++ src/common/cloudflare-image.ts
@@ -1,9 +1,12 @@
+import {Logger} from "@nestjs/common";
+
 export default async function cloudflareImage(imgUrl: string, website: string) {
   const API_TOKEN = process.env.CLOUDFLARE_IMAGE_TOKEN;
   const ACCOUNT_ID = process.env.CLOUDFLARE_ACCOUNT_ID;
   const URL = `https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/images/v1`;
   const DOMAIN = getDomain(website);
   const HASH = process.env.CLOUDFLARE_ACCOUNT_HASH;
+  const logger = new Logger();
 
   const formData = new FormData();
   formData.append("url", imgUrl);
@@ -25,14 +28,14 @@ export default async function cloudflareImage(imgUrl: string, website: string) {
     });
 
     if (response.status !== 200) {
-      this.logger.error("Error uploading image to Cloudflare Image", response);
+      logger.error("Error uploading image to Cloudflare Image", response);
       return null;
     }
 
     const data = await response.json();
     return `https://imagedelivery.net/${HASH}/${data.result.id}`;
   } catch (error) {
-    this.logger.error("Error uploading image to Cloudflare Image", error);
+    logger.error("Error uploading image to Cloudflare Image", error);
     return null;
   }
 }
