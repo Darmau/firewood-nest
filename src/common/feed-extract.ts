c8ea03caac0701f9367ba01e8f210a06c3a86ab7
李大毛 <dev@darmau.design>
2024-01-14 00:26:54 +0800

    fix: logger error

--- src/common/feed-extract.ts
+++ src/common/feed-extract.ts
@@ -1,4 +1,7 @@
+import {Logger} from "@nestjs/common";
+
 export default async function feedExtract(feedUrl: string) {
+  const logger = new Logger();
   try {
     // 请求https://vbwbtqsflwkgwxobfkhs.supabase.co/functions/v1/rss?url=
     let feed = await fetch(`${process.env.SUPABASE_EDGE_FUNCTION}/rss?url=${feedUrl}`, {
@@ -10,7 +13,7 @@ export default async function feedExtract(feedUrl: string) {
     .then((res) => res.json());
     return feed.entries.slice(0, 30);
   } catch (error) {
-    this.logger.error(`Error happen on extract ${feedUrl}: ${error}`);
+    logger.error(`Error happen on extract ${feedUrl}: ${error}`);
     throw new Error(`Error happen on extract ${feedUrl}: ${error}`)
   }
 }
